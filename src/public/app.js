ba390e39f0d4dd02631f3795c848a627cdf337df
dohyun06 <hyun4you2006@naver.com>
2022-05-29 10:47:42 +0900

    Edit tab size

--- src/public/app.js
+++ src/public/app.js
@@ -3,7 +3,7 @@ const dot = ['', '.', '..', '...'];
 let index = 0;
 
 setInterval(() => {
-    h1.innerText = `Loading${dot[index]}`;
-    index += 1;
-    index %= 4;
+  h1.innerText = `Loading${dot[index]}`;
+  index += 1;
+  index %= 4;
 }, 1000);
--- src/server.js
+++ src/server.js
@@ -11,7 +11,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('index.html'));
 app.get('/*', (_, res) => res.redirect('/'));
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     res.writeHead(302, { Location: 'http://ec2-13-209-205-152.ap-northeast-2.compute.amazonaws.com' });
     res.end();
-}).listen(PORT, console.log('http://localhost:3000'));
+  })
+  .listen(PORT, console.log('http://localhost:3000'));
